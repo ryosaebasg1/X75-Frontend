8ca81185006adb4f206c29e6e21396f5b30ff68f
Francois <francois@abcinfo.be>
2025-01-18 12:12:47 +0100

    .

--- register.php
+++ register.php
@@ -1,4 +1,9 @@
 <?php
 
-var_dump($_POST);
+
+// $request = json_decode(json: file_get_contents(filename: 'php://input'));
+$request = file_get_contents(filename: 'php://input');
+
+
+var_dump($request);
 
