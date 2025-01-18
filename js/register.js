0e3d0bf3989fdc2c8079f96cda50fd0f6007bc33
Francois <francois@abcinfo.be>
2025-01-18 11:39:40 +0100

    avec fetch

--- js/register.js
+++ js/register.js
@@ -1,41 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const INPUT_USER_NAME = document.querySelector("#userName");
+  const INPUT_USER_EMAIL = document.querySelector("#userEmail");
+  const INPUT_USER_PASSWORD = document.querySelector("#userpassword");
+  const INPUT_CONFIRM_PASSWORD = document.querySelector("#confirmPassword");
+  const SPAN_MESSAGE = document.querySelector("#message");
 
-const INPUT_USER_NAME = document.querySelector("#userName");
-const INPUT_USER_EMAIL = document.querySelector("#userEmail");
-const INPUT_USER_PASSWORD = document.querySelector("#userpassword");
-const INPUT_CONFIRM_PASSWORD = document.querySelector("#confirmPassword");
-const SPAN_MESSAGE = document.querySelector("#message");
+  const BTN_SEND_FORM = document.querySelector("#btnRegister");
 
-const BTN_SEND_FORM = document.querySelector("#btnRegister");
+  BTN_SEND_FORM.addEventListener("click", (event) => {
+    event.preventDefault();
+    let User = {};
+    User.name = INPUT_USER_NAME.value;
+    User.email = INPUT_USER_EMAIL.value;
+    User.password = INPUT_USER_PASSWORD.value;
+    User.action = "RegsiterNewUser";
+    //Verifeir le password
+    // console.log(User);
 
-BTN_SEND_FORM.addEventListener("click", (event) => {
-  event.preventDefault();
-  User = {};
-  User.name = INPUT_USER_NAME.value;
-  User.email = INPUT_USER_EMAIL.value;
-  User.password = INPUT_USER_PASSWORD.value;
-  User.action = "RegsiterNewUser";
-  console.log(User);
-});
+    // console.log(JSON.stringify(User));
 
-INPUT_USER_EMAIL.addEventListener("keyup", (event) => {
-  console.log("Touche clavier : " + event.key);
-});
+    sendToBack(User);
+  });
 
-INPUT_USER_PASSWORD.addEventListener("focusout", (event) => {
-  console.log("pass");
-});
-INPUT_CONFIRM_PASSWORD.addEventListener("focusout", (event) => {
-  verifPassword(INPUT_USER_PASSWORD.value, INPUT_CONFIRM_PASSWORD.value);
-});
-function verifPassword(password, passwordToVerif) {
-  if (password === passwordToVerif) {
-    SPAN_MESSAGE.style.display = "none";
-  } else {
-    console.log("PAS OK");
-    SPAN_MESSAGE.style.display = "block";
+  INPUT_USER_EMAIL.addEventListener("keyup", (event) => {
+    console.log("Touche clavier : " + event.key);
+  });
+
+  INPUT_USER_PASSWORD.addEventListener("focusout", (event) => {
+    console.log("pass");
+  });
+  INPUT_CONFIRM_PASSWORD.addEventListener("focusout", (event) => {
+    verifPassword(INPUT_USER_PASSWORD.value, INPUT_CONFIRM_PASSWORD.value);
+  });
+  function verifPassword(password, passwordToVerif) {
+    if (password === passwordToVerif) {
+      SPAN_MESSAGE.style.display = "none";
+      console.log("C'est OK");
+    } else {
+      console.log("PAS OK");
+      SPAN_MESSAGE.style.display = "block";
+    }
   }
-}
+
+  function sendToBack(user) {
+    console.log(user);
+    fetch("register.php", {
+      method: "POST",
+      //   headers: {
+      //     "content-type": "application/json",
+      //   },
+      //   body: JSON.stringify(user),
+      body: {
+        userName: user.name,
+      },
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+});
+
 /*
 ToDo : 
 - Ajouter le texte html
--- register.php
+++ register.php
@@ -2,30 +2,3 @@
 
 var_dump($_POST);
 
-
-
-
-
-
-
-
-
-
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>:skjdfhkdfghkjsd</p>
-    <p>:skjdfh - <?php echo $_POST["Choix_de_l'option"]; ?> - kdfghkjsd</p>
-    <p>:skjdfhkdfghkjsd</p>
-    <p>:skjdfhkdfghkjsd</p>
-</body>
-</html>
